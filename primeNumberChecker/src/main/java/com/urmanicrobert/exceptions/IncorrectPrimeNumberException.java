aca8a2e1261a125dfb7915fff23a045a0401d6f7
Robert-Urmanic <91885141+Robert-Urmanic@users.noreply.github.com>
2024-04-15 16:47:17 +0200

    Add files via upload

---
+++ primeNumberChecker/src/main/java/com/urmanicrobert/exceptions/IncorrectPrimeNumberException.java
@@ -0,0 +1,7 @@
+package com.urmanicrobert.exceptions;
+
+public class IncorrectPrimeNumberException extends Exception{
+    public IncorrectPrimeNumberException(String errorMessage){
+        super(errorMessage);
+    }
+}
